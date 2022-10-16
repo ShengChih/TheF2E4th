d87d4d7286f4c9149f8c90b35827c912412e833c
palapalado <ms0529756@gmail.com>
2022-10-16 03:32:13 +0000

    initialize postcss-px-to-vw plugin to support RWD on different devices.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,25 @@ import TaiwanLogoImage from "./TaiwanLogo.svg";
 import FirstNavIcon from "./FirstNavIcon.svg";
 import SecondNavIcon from "./SecondNavIcon.svg";
 import ThirdNavIcon from "./ThirdNavIcon.svg";
+// import { isDesktop, isTablet, isMobile } from 'react-device-detect';
+
+import './Header.scss'
+
+/**
+ * Don't use sass/scss @import for different devices classnames, 
+ * because that causes postcss-px-to-vw packages
+ * execute in wrong ordered postcss plugins
+ * 
+ * 因為 sass/scss 透過 postcss-import 支援 @import 語法
+ * 因此 postcss-import 先於 postcss-px-to-vw 將所有的 sass/scss 檔案匯聚成一份
+ * 就不能使用 postcss-px-to-vw exclude / include 的設定根據不同 designedWidth 設定 config
+ * px 轉 vw 就會只用最先執行的 config 轉。
+ * 
+ * 這只會限制特別的裝置 styles，針對 media query 內的用法，其他不在意可使用 @import
+ */
+import './pc/device.scss';
+import './mobile/device.scss';
+import './tablet/device.scss';
 
 const items = [
   {
@@ -27,7 +46,7 @@ const items = [
 
 function Header() {
   return (
-    <div className="w-full max-w-[1280px] h-[104px] outline-show mx-auto relative">
+    <div className="outline-show header header--mobile header--tablet header--pc">
       <div
         style={{
           backgroundImage: `url(${TaiwanLogoImage})`
@@ -35,8 +54,8 @@ function Header() {
         className={'w-[99px] h-[57px] left-[108px] top-[29px] outline-show absolute bg-no-repeat bg-cover'}
       />
       <div className="outline-show flex absolute left-[824px] top-[54px]">
-        {items.map(({ itemLink, itemColor, linkText, backgroundImg }) => (
-          <a href={itemLink} className="outline-show left-[125px] top-[34px] flex items-end">
+        {items.map(({ itemLink, itemColor, linkText, backgroundImg }, index) => (
+          <a key={`item-${index}`}  href={itemLink} className="outline-show left-[125px] top-[34px] flex items-end">
             <div
               style={{
                 backgroundImage: `url(${backgroundImg})`
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,13 @@ import { viteMockServe } from 'vite-plugin-mock';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 import path from 'path';
-
+import postcssImport from 'postcss-import';
+import tailwindcssNesting from 'tailwindcss/nesting';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import px2vw from 'postcss-px-to-viewport'
+import px2rem from 'postcss-pxtorem'
+import postcssNesting from 'postcss-nesting'
 
 export default defineConfig(({ command }: ConfigEnv) => {
   return {
@@ -79,6 +85,69 @@ export default defineConfig(({ command }: ConfigEnv) => {
             react: ['react', 'react-dom', 'styled-components'],
           }
         }
+      }
+    },
+    css: {
+      postcss: {
+        plugins: [
+          postcssImport,
+          tailwindcssNesting(postcssNesting),
+          tailwindcss,
+          autoprefixer,
+          px2vw({
+            unitToConvert: 'px', // 要轉化的單位
+            viewportWidth: 360, // UI設計稿的寬度
+            unitPrecision: 6, // 轉換後的精度，即小數點位數
+            propList: ['*', '!min-width', '!min-height'], // 指定轉換的css屬性的單位，*代表全部css屬性的單位都進行轉換
+            viewportUnit: 'vw', // 指定需要轉換成的視窗單位，默認vw
+            fontViewportUnit: 'vw', // 指定字體需要轉換成的視窗單位，默認vw
+            selectorBlackList: ['wrap'], // 指定不轉換為視窗單位的類名，
+            minPixelValue: 1, // 默認值1，小於或等於4px則不進行轉換
+            mediaQuery: true, // 是否在媒體查詢的css代碼中也進行轉換，默認false
+            replace: true, // 是否轉換後直接更換屬性值
+            exclude: [/node_modules/, /pc/, /tablet/], // 設置忽略文件，用正則做目錄名匹配
+            include: [/mobile/],
+            landscape: false // 是否處理橫屏情況
+          }),
+          px2vw({
+            unitToConvert: 'px', // 要轉化的單位
+            viewportWidth: 1280, // UI設計稿的寬度
+            unitPrecision: 6, // 轉換後的精度，即小數點位數
+            propList: ['*', '!min-width', '!min-height'], // 指定轉換的css屬性的單位，*代表全部css屬性的單位都進行轉換
+            viewportUnit: 'vw', // 指定需要轉換成的視窗單位，默認vw
+            fontViewportUnit: 'vw', // 指定字體需要轉換成的視窗單位，默認vw
+            selectorBlackList: ['wrap'], // 指定不轉換為視窗單位的類名，
+            minPixelValue: 1, // 默認值1，小於或等於4px則不進行轉換
+            mediaQuery: true, // 是否在媒體查詢的css代碼中也進行轉換，默認false
+            replace: true, // 是否轉換後直接更換屬性值
+            exclude: [/node_modules/, /mobile/, /tablet/], // 設置忽略文件，用正則做目錄名匹配
+            landscape: false // 是否處理橫屏情況
+          }),
+          px2vw({
+            unitToConvert: 'px', // 要轉化的單位
+            viewportWidth: 768, // UI設計稿的寬度
+            unitPrecision: 6, // 轉換後的精度，即小數點位數
+            propList: ['*', '!min-width', '!min-height'], // 指定轉換的css屬性的單位，*代表全部css屬性的單位都進行轉換
+            viewportUnit: 'vw', // 指定需要轉換成的視窗單位，默認vw
+            fontViewportUnit: 'vw', // 指定字體需要轉換成的視窗單位，默認vw
+            selectorBlackList: ['wrap'], // 指定不轉換為視窗單位的類名，
+            minPixelValue: 1, // 默認值1，小於或等於4px則不進行轉換
+            mediaQuery: true, // 是否在媒體查詢的css代碼中也進行轉換，默認false
+            replace: true, // 是否轉換後直接更換屬性值
+            exclude: [/node_modules/, /mobile/, /pc/], // 設置忽略文件，用正則做目錄名匹配
+            landscape: false // 是否處理橫屏情況
+          }),
+          px2rem({
+            rootValue: 16,
+            unitPrecision: 6,
+            propList: ['font', 'font-size', 'line-height', 'letter-spacing'],
+            selectorBlackList: [],
+            replace: true,
+            mediaQuery: false,
+            minPixelValue: 4,
+            exclude: /node_modules/
+          })
+        ]
       }
     }
   };
