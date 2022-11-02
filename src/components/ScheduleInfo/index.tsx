d9cd148b548ce1b6abbec9f481db70a728b49825
palapalado <ms0529756@gmail.com>
2022-11-02 07:53:41 +0000

    fix newline and react key warning, and add schedule line

--- src/components/ScheduleInfo/index.tsx
+++ src/components/ScheduleInfo/index.tsx
@@ -19,35 +19,35 @@ function SchedulePoint({
 		<div className={`relative flex items-center flex-col ${CardStyle}`}>
 			<div className={`relative font-sans text-[#38241B] desktop:leading-[43px] desktop:text-[30px] desktop:top-[-63px]`}>{title}</div>
 			<div className={`rotate-45 bg-[#3C221B] desktop:w-[48.53px] desktop:h-[48.53px] ${RectangleStyle}`}></div>
-			<div className={`relative font-sans text-[#38241B] ${PeriodStyle} desktop:leading-[35px] desktop:text-[24px] desktop:top-[32.37px]`}>{ActivityPeriods}</div>
+			<div className={`relative whitespace-pre-line	font-sans text-[#38241B] ${PeriodStyle} desktop:leading-[35px] desktop:text-[24px] desktop:top-[32.37px]`}>{ActivityPeriods}</div>
 		</div>
 	)
 }
 
-const SchedulePoints = [
+const ProgressPoints = [
 	{
-		CardStyle: `desktop:w-[290.37px]`,
+		CardStyle: `desktop:w-[323px]`,
 		title: `開始報名`,
 		RectangleStyle: ``,
 		PeriodStyle: `desktop:leading-[35px] desktop:text-[24px] desktop:top-[32.37px]`,
-		ActivityPeriods: ['10/13(四) 早上 11:00', <br />, '至 11/6(日) 晚上 23:59']
+		ActivityPeriods: '10/13(四) 早上 11:00\n至 11/6(日) 晚上 23:59'
 	},
 	{
-		CardStyle: `desktop:w-[290.37px]`,
+		CardStyle: `desktop:w-[323px]`,
 		title: `開賽`,
 		RectangleStyle: ``,
 		PeriodStyle: `desktop:leading-[35px] desktop:text-[24px] desktop:top-[32.37px]`,
-		ActivityPeriods: ['UI組、團體組開賽 10/31', <br />, '前端組開賽 11 /7'],
+		ActivityPeriods: 'UI組、團體組開賽 10/31\n前端組開賽 11 /7',
 	},
 	{
-		CardStyle: `desktop:w-[290.37px]`,
+		CardStyle: `desktop:w-[323px]`,
 		title: `投稿作品`,
 		RectangleStyle: ``,
 		PeriodStyle: `desktop:leading-[35px] desktop:text-[24px] desktop:top-[32.37px]`,
-		ActivityPeriods: ['10/31(一) 中午 12:00', <br />, '至11/28(一) 中午 12:00'],
+		ActivityPeriods: '10/31(一) 中午 12:00\n至11/28(一) 中午 12:00',
 	},
 	{
-		CardStyle: `desktop:w-[290.37px]`,
+		CardStyle: `desktop:w-[323px]`,
 		title: `線上直播`,
 		RectangleStyle: ``,
 		PeriodStyle: `desktop:leading-[35px] desktop:text-[24px] desktop:top-[48.37px]`,
@@ -55,16 +55,16 @@ const SchedulePoints = [
 	}
 ]
 
-const SchedulePoints2 = [
+const FinalPoints = [
 	{
-		CardStyle: `desktop:w-[290.37px]`,
+		CardStyle: `desktop:w-[317px]`,
 		title: `初選`,
 		RectangleStyle: ``,
 		PeriodStyle: `desktop:leading-[35px] desktop:text-[24px] desktop:top-[48.37px]`,
 		ActivityPeriods: '12/05(五)',
 	},
 	{
-		CardStyle: `desktop:w-[284.37px]`,
+		CardStyle: `desktop:w-[317px]`,
 		title: `決選`,
 		RectangleStyle: ``,
 		PeriodStyle: `desktop:leading-[35px] desktop:text-[24px] desktop:top-[48.37px]`,
@@ -76,15 +76,21 @@ export default function ScheduleInfo() {
 	return (
 		<section className={`w-full desktop:h-[1040px]`}>
 			<div className={`flex items-center justify-center bg-[#3C221B] font-serif font-black text-white desktop:h-[170px] desktop:text-[60px] desktop:leading-[86px]`}>賽程時間</div>
-			<div className={`flex mx-auto w-max desktop:mt-[155px]`}>
-				{
-					SchedulePoints.map((point, index:number ) => <SchedulePoint {...point} key={`scheduleponit1-key-${index}`} />)
-				}
+			<div className={`relative`}>
+				<div className={`absolute left-[151px] border-[5px] border-solid border-[#3C221B] desktop:left-[151px] desktop:top-[63px] desktop:w-[957px]`}></div>
+				<div className={`flex mx-auto w-max desktop:mt-[155px]`}>
+					{
+						ProgressPoints.map((point, index:number ) => <SchedulePoint key={`progress-${index}`} {...point} />)
+					}
+				</div>
 			</div>
-			<div className={`flex mx-auto w-max desktop:mt-[155px]`}>
-				{
-					SchedulePoints2.map((point, index:number ) => <SchedulePoint {...point} key={`scheduleponit2-key-${index}`} />)
-				}
+			<div className={`relative`}>
+				<div className={`absolute left-[151px] border-[5px] border-solid border-[#3C221B] desktop:left-[466px] desktop:top-[63px] desktop:w-[328px]`}></div>
+				<div className={`flex mx-auto w-max desktop:mt-[155px]`}>
+					{
+						FinalPoints.map((point, index: number) => <SchedulePoint key={`final-${index}`} {...point} />)
+					}
+				</div>
 			</div>
 			<div className={`font-sans text-[#3C221B] desktop:text-[28px] mx-auto desktop:w-[1198px] desktop:h-[89px] desktop:mt-[127.37px]`}>
 				初選：將由六角學院前端、UI 評審進行第一波篩選，並於 12/5（五）公布初選佳作名單。<br />
