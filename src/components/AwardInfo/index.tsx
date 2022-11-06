a734fb2ae66c2e344f7143336a500bbbf467f71c
palapalado <ms0529756@gmail.com>
2022-11-06 01:59:25 +0000

    fix(newline): fix newline

--- src/components/AwardInfo/index.tsx
+++ src/components/AwardInfo/index.tsx
@@ -68,7 +68,7 @@ const AwardInfoBase: ForwardRefRenderFunction<AwardInfoHandle, AwardInfoProps> =
 					>
 						<div className={`relative mx-auto shadow-[0_4px_4px_rgba(0,0,0,0.25)] font-bold font-sans bg-[#951205] text-white flex items-center justify-center desktop:w-[232.41px] desktop:h-[61.5px] desktop:rounded-[9px] desktop:top-[292.37px] desktop:text-[30px] desktop:leading-[43.44px]`}>$3000 (6名)</div>
 					</div>
-					<div className={`font-sans font-normal whitespace-pre text-center text-[#3C221B] desktop:text-[26px] desktop:leading-[38px]`}>每個關卡各 2 名，\n設計 1 名、前端 1 名</div>
+					<div className={`font-sans font-normal whitespace-pre text-center text-[#3C221B] desktop:text-[26px] desktop:leading-[38px]`}>{'每個關卡各 2 名，\n設計 1 名、前端 1 名'}</div>
 				</div>
 				<div className={`relative desktop:w-[386px] desktop:h-[461px]`}>
 					<div
@@ -80,7 +80,7 @@ const AwardInfoBase: ForwardRefRenderFunction<AwardInfoHandle, AwardInfoProps> =
 					>
 						<div className={`relative mx-auto shadow-[0_4px_4px_rgba(0,0,0,0.25)] font-bold font-sans bg-[#951205] text-white flex items-center justify-center desktop:w-[233.02px] desktop:h-[61.67px] desktop:rounded-[9px] desktop:top-[292.18px] desktop:text-[30px]`}>數位獎狀(60名)</div>
 					</div>
-					<div className={`font-sans font-normal text-center text-[#3C221B] desktop:text-[26px] desktop:leading-[38px] desktop:mt-[11px]`}>每個關卡個人組十位\n、團體組十組</div>
+					<div className={`font-sans font-normal text-center text-[#3C221B] desktop:text-[26px] desktop:leading-[38px] desktop:mt-[11px]`}>{'每個關卡個人組十位\n、團體組十組'}</div>
 				</div>
 			</div>
 			<div className={`font-sans font-normal text-center mx-auto text-[#3C221B] desktop:w-[396px] desktop:h-[52px] desktop:leading-[52px] desktop:text-[36px] desktop:mt-[92px] desktop:mb-[12px]`}>以上皆提供完賽數位獎狀</div>
