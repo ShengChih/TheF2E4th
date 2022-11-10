f062e34e22c98fab76c59d43dfda132199c52c4e
palapalado <ms0529756@gmail.com>
2022-11-10 09:25:41 +0000

    add tablet mask styles

--- src/pages/MainPage/type.d.ts
+++ src/pages/MainPage/type.d.ts
@@ -1,6 +1,9 @@
 import { MouseEvent } from 'react'
+import { gsap } from "@animations/gsap"
 
 export type MainPageHandle = {
 	gotoHexSchoolAnchor: (e: MouseEvent) => void
 	gotoScheduleInfoAnchor: (e: MouseEvent) => void
 }
+
+export type AnimationReturn = ReturnType<typeof gsap.context | typeof gsap.timeline | typeof gsap.fromTo>
