23f5e955c21baf4a97563e0799fca1d673a6b880
palapalado <ms0529756@gmail.com>
2022-11-11 15:47:00 +0000

    adjust schedule info card style on tablet

--- src/components/ScheduleInfo/index.tsx
+++ src/components/ScheduleInfo/index.tsx
@@ -18,12 +18,24 @@ function SchedulePoint({
 	ActivityPeriods
 }: SchedulePointProps) {
 	return (
-		<div className={`relative flex items-center flex-col ${CardStyle}`}>
-			<div className={`relative font-sans text-[#38241B] xl:leading-[43px] xl:text-[30px]`}>{title}</div>
-			<div className={`flex items-center justify-center xl:mt-[20px] xl:w-[68.37px] xl:h-[68.37px]`}>
-				<div className={`rotate-45 xl:w-[48.53px] xl:h-[48.53px] ${RectangleStyle}`}></div>
+		<div className={flatClassName({
+			common: `relative flex items-center flex-col ${CardStyle}`,
+
+		})}>
+			<div className={flatClassName({
+				common: `relative font-sans text-[#38241B] leading-[43px] text-[30px]`,
+				desktop: ``
+			})}>{title}</div>
+			<div className={flatClassName({
+				common: `flex items-center justify-center mt-[20px] w-[68.37px] h-[68.37px]`
+			})}>
+				<div className={flatClassName({
+					common: `rotate-45 w-[48.53px] h-[48.53px] ${RectangleStyle}`
+				})}></div>
 			</div>
-			<div className={`relative grid font-sans font-normal text-[#38241B] xl:leading-[35px] xl:text-[24px] ${PeriodStyle}`}>
+			<div className={flatClassName({
+				common: `relative grid font-sans font-normal text-[#38241B] leading-[35px] text-[24px] ${PeriodStyle}`
+			})}>
 				{
 					ActivityPeriods.map((text: string, index: number) => {
 						return <div key={`${index}`}>{text}</div>
@@ -36,37 +48,53 @@ function SchedulePoint({
 
 const ProgressPoints = [
 	{
-		CardStyle: `xl:w-[320px]`,
+		CardStyle: flatClassName({
+			common: `w-[320px]`
+		}),
 		title: `開始報名`,
 		RectangleStyle: ``,
-		PeriodStyle: `content-between xl:mt-[32.37px] xl:h-[82px] `,
+		PeriodStyle: flatClassName({
+			common: `content-between mt-[32.37px] h-[82px] `
+		}),
 		ActivityPeriods: ['10/13(四) 早上 11:00', '至 11/6(日) 晚上 23:59'],
 		starttime: +new Date("2022-10-13T11:00:00.000+08:00"),
 		endtime: +new Date("2022-11-06T23:59:59.000+08:00")
 	},
 	{
-		CardStyle: `xl:w-[320px]`,
+		CardStyle: flatClassName({
+			common: `w-[320px]`
+		}),
 		title: `開賽`,
 		RectangleStyle: ``,
-		PeriodStyle: `content-between xl:mt-[32.37px] xl:h-[82px]`,
+		PeriodStyle: flatClassName({
+			common: `content-between mt-[32.37px] h-[82px] `
+		}),
 		ActivityPeriods: ['UI組、團體組開賽 10/31', '前端組開賽 11 /7'],
 		starttime: +new Date("2022-10-31T00:00:00.000+08:00"),
 		endtime: +new Date("2022-11-07T23:59:59.000+08:00")
 	},
 	{
-		CardStyle: `xl:w-[320px]`,
+		CardStyle: flatClassName({
+			common: `w-[320px]`
+		}),
 		title: `投稿作品`,
 		RectangleStyle: ``,
-		PeriodStyle: `content-between xl:mt-[32.37px] xl:h-[82px]`,
+		PeriodStyle: flatClassName({
+			common: `content-between mt-[32.37px] h-[82px] `
+		}),
 		ActivityPeriods: ['10/31(一) 中午 12:00', '至11/28(一) 中午 12:00'],
 		starttime: +new Date("2022-10-31T12:00:00.000+08:00"),
 		endtime: +new Date("2022-11-28T12:00:00.000+08:00")
 	},
 	{
-		CardStyle: `xl:w-[320px]`,
+		CardStyle: flatClassName({
+			common: `w-[320px]`
+		}),
 		title: `線上直播`,
 		RectangleStyle: ``,
-		PeriodStyle: `content-center xl:mt-[48.37px] xl:h-[35px]`,
+		PeriodStyle: flatClassName({
+			common: `content-between mt-[48.37px] h-[35px]`
+		}),
 		ActivityPeriods: ['11/3 至 11/24(每週四)'],
 		starttime: +new Date("2022-11-03T00:00:00.000+08:00"),
 		endtime: +new Date("2022-11-24T23:59:59.000+08:00")
@@ -75,19 +103,27 @@ const ProgressPoints = [
 
 const FinalPoints = [
 	{
-		CardStyle: `xl:w-[320px]`,
+		CardStyle: flatClassName({
+			common: `w-[320px]`
+		}),
 		title: `初選`,
 		RectangleStyle: ``,
-		PeriodStyle: `xl:mt-[48.37px]`,
+		PeriodStyle: flatClassName({
+			common: `xl:mt-[48.37px]`
+		}),
 		ActivityPeriods: ['12/05(五)'],
 		starttime: +new Date("2022-12-05T00:00:00.000+08:00"),
 		endtime: +new Date("2022-12-05T23:59:59.000+08:00")
 	},
 	{
-		CardStyle: `xl:w-[320px]`,
+		CardStyle: flatClassName({
+			common: `w-[320px]`
+		}),
 		title: `決選`,
 		RectangleStyle: ``,
-		PeriodStyle: `xl:mt-[48.37px]`,
+		PeriodStyle: flatClassName({
+			common: `xl:mt-[48.37px]`
+		}),
 		ActivityPeriods: ['12/05(五)'],
 		starttime: +new Date("2022-12-05T00:00:00.000+08:00"),
 		endtime: +new Date("2022-12-05T23:59:59.000+08:00")
@@ -107,37 +143,75 @@ export default function ScheduleInfo({ children }: ScheduleInfoProps) {
 			})} title={`賽程時間`} />
 			<div className={flatClassName({
 				common: `relative`,
-				desktop: `xl:flex xl:items-center xl:items-center xl:justify-center xl:h-[870px]`,
-				tablet: `h-[114px]`,
+				desktop: `xl:flex xl:items-center xl:justify-center xl:h-[870px]`,
+				tablet: `md:h-[617px]`,
 				mobile: ``
 			})}>
 				<div className={flatClassName({
 					common: `relative `,
 					desktop: `xl:w-[1280px] xl:h-[686px]`,
-					tablet: ``,
+					tablet: `md:h-[487px]`,
 					mobile: ``
 				})}>
 					<div className={flatClassName({
-						common: `relative flex xl:w-[1280px]  xl:h-[246px]`,
-						desktop: ``,
-						tablet: ``,
+						tablet: `md:flex md:flex-nowrap md:my-[66px] md:w-[1667px] md:h-[246px]`
 					})}>
 						<div className={flatClassName({
-							common: `absolute inset-x-0 flex items-center mx-auto`,
-							desktop: `xl:mt-[63px] xl:w-[960px] xl:h-[68.37px]`,
+							common: `relative flex w-[1280px]  h-[246px]`,
+							desktop: ``,
 							tablet: ``,
-							mobile: ``,
 						})}>
 							<div className={flatClassName({
-								common: `absolute inset-x-0 mx-auto border-[5px] border-solid border-[#951205]`,
+								common: `absolute inset-x-0 flex items-center mx-auto`,
+								desktop: `mt-[63px] w-[960px] h-[68.37px]`,
+								tablet: ``,
+								mobile: ``,
+							})}>
+								<div className={flatClassName({
+									common: `absolute inset-x-0 mx-auto border-[5px] border-solid border-[#951205]`,
+									desktop: ``,
+									tablet: ``,
+									mobile: ``
+								})}></div>
+							</div>
+							{
+								ProgressPoints.map(
+									({ starttime, endtime, RectangleStyle, ...point }, index: number) => {
+										const now = + new Date()
+										const rectangleStyle = RectangleStyle + (
+											now >= starttime
+											? `bg-[#951205]`
+											: `bg-[#3C221B]`
+										)
+										return (
+											<SchedulePoint
+												key={`progress-${index}`}
+												RectangleStyle={rectangleStyle}
+												{ ...point }
+											/>
+										)
+									}
+								)
+							}
+						</div>
+						<div className={flatClassName({
+							common: `relative mx-auto flex`,
+							desktop: `xl:w-[640px] xl:mt-[92px] xl:h-[215px]`,
+							tablet: ``,
+							mobile: ``
+						})}>
+							<div className={flatClassName({
+								common: `absolute inset-x-0 flex items-center mx-auto xl:mt-[63px] xl:w-[320px] xl:h-[68.37px]`,
 								desktop: ``,
 								tablet: ``,
-								mobile: ``
-							})}></div>
-						</div>
-						{
-							ProgressPoints.map(
-								({ starttime, endtime, RectangleStyle, ...point }, index: number) => {
+								mobile: ``,
+							})}>
+								<div className={flatClassName({
+									common: `absolute inset-x-0 mx-auto border-[5px] border-solid ${+new Date() >= +new Date("2022-12-05T00:00:00.000+08:00") ? 'border-[#951205]' : 'border-[#3C221B]'}`,
+								})}></div>
+							</div>
+							{
+								FinalPoints.map(({ starttime, endtime, RectangleStyle, ...point }, index: number) => {
 									const now = + new Date()
 									const rectangleStyle = RectangleStyle + (
 										now >= starttime
@@ -146,53 +220,19 @@ export default function ScheduleInfo({ children }: ScheduleInfoProps) {
 									)
 									return (
 										<SchedulePoint
-											key={`progress-${index}`}
+											key={`final-${index}`}
 											RectangleStyle={rectangleStyle}
-											{ ...point }
+											{...point}
 										/>
 									)
-								}
-							)
-						}
-					</div>
-					<div className={flatClassName({
-						common: `relative mx-auto flex`,
-						desktop: `xl:w-[640px] xl:mt-[92px] xl:h-[215px]`,
-						tablet: ``,
-						mobile: ``
-					})}>
-						<div className={flatClassName({
-							common: `absolute inset-x-0 flex items-center mx-auto xl:mt-[63px] xl:w-[320px] xl:h-[68.37px]`,
-							desktop: ``,
-							tablet: ``,
-							mobile: ``,
-						})}>
-							<div className={flatClassName({
-								common: `absolute inset-x-0 mx-auto border-[5px] border-solid ${+new Date() >= +new Date("2022-12-05T00:00:00.000+08:00") ? 'border-[#951205]' : 'border-[#3C221B]'}`,
-							})}></div>
+								})
+							}
 						</div>
-						{
-							FinalPoints.map(({ starttime, endtime, RectangleStyle, ...point }, index: number) => {
-								const now = + new Date()
-								const rectangleStyle = RectangleStyle + (
-									now >= starttime
-									? `bg-[#951205]`
-									: `bg-[#3C221B]`
-								)
-								return (
-									<SchedulePoint
-										key={`final-${index}`}
-										RectangleStyle={rectangleStyle}
-										{...point}
-									/>
-								)
-							})
-						}
 					</div>
 					<div className={flatClassName({
 						common: `font-sans mx-auto whitespace-pre-line text-[#3C221B] `,
-						desktop: `xl:text-[28px]  xl:w-[1198px] xl:h-[89px] xl:mt-[44px]`,
-						tablet: ``,
+						desktop: `text-[28px]  xl:w-[1198px] xl:h-[89px] xl:mt-[44px]`,
+						tablet: `md:w-[736px] md:h-[175px] md:mb-[64px]`,
 						mobile: ``
 					})}>
 						{
