e80e5ad28e3607b549398f7ec5e7cc522678dcc0
palapalado <ms0529756@gmail.com>
2022-11-12 10:53:43 +0000

    fix fonts is droped issue

--- scripts/sh/extract_font.sh
+++ scripts/sh/extract_font.sh
@@ -5,6 +5,7 @@ docker run --rm -i -t \
 	pyfonttools:latest \
 	pyftsubset \
 	--text="0123456789$," \
+	--passthrough-tables \
 /usr/src/app/fonts/EBGaramond/EBGaramond-Bold.otf --output-file="/usr/src/app/fonts/EBGaramond/small-EBGaramond-Bold.otf"
 
 echo "/usr/src/app/fonts/ar-julian.woff\n"
@@ -13,6 +14,7 @@ docker run --rm -i -t \
 	pyfonttools:latest \
 	pyftsubset \
 	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載中.網站中收集五顆散落的柏蒂豆，即可獲得驚喜唷立競賽說明關卡任務" \
+	--passthrough-tables \
 /usr/src/app/fonts/ar-julian.woff --output-file="/usr/src/app/fonts/small-ar-julian.woff"
 
 echo "/usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Black.otf"
@@ -21,6 +23,7 @@ docker run --rm -i -t \
 	pyfonttools:latest \
 	pyftsubset \
 	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載中.網站中收集五顆散落的柏蒂豆，即可獲得驚喜唷立競賽說明關卡任務" \
+	--passthrough-tables \
 /usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Black.otf --output-file="/usr/src/app/fonts/Noto_Sans_TC/small-NotoSansTC-Black.otf"
 
 echo "/usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Bold.otf"
@@ -29,6 +32,7 @@ docker run --rm -i -t \
 	pyfonttools:latest \
 	pyftsubset \
 	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載中.網站中收集五顆散落的柏蒂豆，即可獲得驚喜唷立競賽說明關卡任務" \
+	--passthrough-tables \
 /usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Bold.otf --output-file="/usr/src/app/fonts/Noto_Sans_TC/small-NotoSansTC-Bold.otf"
 
 echo "/usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Light.otf\n"
@@ -37,6 +41,7 @@ docker run --rm -i -t \
 	pyfonttools:latest \
 	pyftsubset \
 	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載中.網站中收集五顆散落的柏蒂豆，即可獲得驚喜唷立競賽說明關卡任務" \
+	--passthrough-tables \
 /usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Light.otf --output-file="/usr/src/app/fonts/Noto_Sans_TC/small-NotoSansTC-Light.otf"
 
 echo "/usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Medium.otf"
@@ -45,6 +50,7 @@ docker run --rm -i -t \
 	pyfonttools:latest \
 	pyftsubset \
 	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載中.網站中收集五顆散落的柏蒂豆，即可獲得驚喜唷立競賽說明關卡任務" \
+	--passthrough-tables \
 /usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Medium.otf --output-file="/usr/src/app/fonts/Noto_Sans_TC/small-NotoSansTC-Medium.otf"
 
 echo "/usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Regular.otf\n"
@@ -53,6 +59,7 @@ docker run --rm -i -t \
 	pyfonttools:latest \
 	pyftsubset \
 	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載中.網站中收集五顆散落的柏蒂豆，即可獲得驚喜唷立競賽說明關卡任務" \
+	--passthrough-tables \
 /usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Regular.otf --output-file="/usr/src/app/fonts/Noto_Sans_TC/small-NotoSansTC-Regular.otf"
 
 echo "/usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Thin.otf\n"
@@ -61,6 +68,7 @@ docker run --rm -i -t \
 	pyfonttools:latest \
 	pyftsubset \
 	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載中.網站中收集五顆散落的柏蒂豆，即可獲得驚喜唷立競賽說明關卡任務" \
+	--passthrough-tables \
 /usr/src/app/fonts/Noto_Sans_TC/NotoSansTC-Thin.otf --output-file="/usr/src/app/fonts/Noto_Sans_TC/small-NotoSansTC-Thin.otf"
 
 echo "/usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-Black.otf\n"
@@ -69,6 +77,7 @@ docker run --rm -i -t \
 	pyfonttools:latest \
 	pyftsubset \
 	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載中.網站中收集五顆散落的柏蒂豆，即可獲得驚喜唷立競賽說明關卡任務" \
+	--passthrough-tables \
 /usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-Black.otf --output-file="/usr/src/app/fonts/Noto_Serif_TC/small-NotoSerifTC-Black.otf"
 
 echo "/usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-Medium.otf\n"
@@ -77,6 +86,7 @@ docker run --rm -i -t \
 	pyfonttools:latest \
 	pyftsubset \
 	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載中.網站中收集五顆散落的柏蒂豆，即可獲得驚喜唷立競賽說明關卡任務" \
+	--passthrough-tables \
 /usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-Medium.otf --output-file="/usr/src/app/fonts/Noto_Serif_TC/small-NotoSerifTC-Medium.otf"
 
 echo "/usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-Regular.otf\n"
@@ -85,6 +95,7 @@ docker run --rm -i -t \
 	pyfonttools:latest \
 	pyftsubset \
 	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載中.網站中收集五顆散落的柏蒂豆，即可獲得驚喜唷立競賽說明關卡任務" \
+	--passthrough-tables \
 /usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-Regular.otf --output-file="/usr/src/app/fonts/Noto_Serif_TC/small-NotoSerifTC-Regular.otf"
 
 echo "/usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-SemiBold.otf\n"
@@ -93,6 +104,7 @@ docker run --rm -i -t \
 	pyfonttools:latest \
 	pyftsubset \
 	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載中.網站中收集五顆散落的柏蒂豆，即可獲得驚喜唷立競賽說明關卡任務" \
+	--passthrough-tables \
 /usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-SemiBold.otf --output-file="/usr/src/app/fonts/Noto_Serif_TC/small-NotoSerifTC-SemiBold.otf"
 
 echo "/usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-Bold.otf"
@@ -101,6 +113,7 @@ docker run --rm -i -t \
 	pyfonttools:latest \
 	pyftsubset \
 	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載中.網站中收集五顆散落的柏蒂豆，即可獲得驚喜唷立競賽說明關卡任務" \
+	--passthrough-tables \
 /usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-Bold.otf --output-file="/usr/src/app/fonts/Noto_Serif_TC/small-NotoSerifTC-Bold.otf"
 
 echo "/usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-ExtraLight.otf\n"
@@ -109,6 +122,7 @@ docker run --rm -i -t \
 	pyfonttools:latest \
 	pyftsubset \
 	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載中.網站中收集五顆散落的柏蒂豆，即可獲得驚喜唷立競賽說明關卡任務" \
+	--passthrough-tables \
 /usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-ExtraLight.otf --output-file="/usr/src/app/fonts/Noto_Serif_TC/small-NotoSerifTC-ExtraLight.otf"
 
 echo "/usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-Light.otf\n"
@@ -117,6 +131,7 @@ docker run --rm -i -t \
 	pyfonttools:latest \
 	pyftsubset \
 	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載中.網站中收集五顆散落的柏蒂豆，即可獲得驚喜唷立競賽說明關卡任務" \
+	--passthrough-tables \
 /usr/src/app/fonts/Noto_Serif_TC/NotoSerifTC-Light.otf --output-file="/usr/src/app/fonts/Noto_Serif_TC/small-NotoSerifTC-Light.otf"
 
 echo "/usr/src/app/fonts/Roboto/Roboto-Regular.otf\n"
@@ -125,6 +140,7 @@ docker run --rm -i -t \
 	pyfonttools:latest \
 	pyftsubset \
 	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載中.網站中收集五顆散落的柏蒂豆，即可獲得驚喜唷立競賽說明關卡任務" \
+	--passthrough-tables \
 /usr/src/app/fonts/Roboto/Roboto-Regular.otf --output-file="/usr/src/app/fonts/Roboto/small-Roboto-Regular.otf"
 
 echo "/usr/src/app/fonts/Roboto/Roboto-Medium.otf\n"
@@ -133,6 +149,7 @@ docker run --rm -i -t \
 	pyfonttools:latest \
 	pyftsubset \
 	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載中.網站中收集五顆散落的柏蒂豆，即可獲得驚喜唷立競賽說明關卡任務" \
+	--passthrough-tables \
 /usr/src/app/fonts/Roboto/Roboto-Medium.otf --output-file="/usr/src/app/fonts/Roboto/small-Roboto-Medium.otf"
 
 echo "/usr/src/app/fonts/Roboto/Roboto-Black.otf\n"
@@ -141,6 +158,7 @@ docker run --rm -i -t \
 	pyfonttools:latest \
 	pyftsubset \
 	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載中.網站中收集五顆散落的柏蒂豆，即可獲得驚喜唷立競賽說明關卡任務" \
+	--passthrough-tables \
 /usr/src/app/fonts/Roboto/Roboto-Black.otf --output-file="/usr/src/app/fonts/Roboto/small-Roboto-Black.otf"
 
 echo "/usr/src/app/fonts/Roboto/Roboto-Bold.otf\n"
@@ -149,6 +167,7 @@ docker run --rm -i -t \
 	pyfonttools:latest \
 	pyftsubset \
 	--text="0123456789AaBbCcDdEeFfGgHhIiKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz意想不到的好康 請點擊以上皆提供完賽數位獎狀每個關卡各組名，設計、前端人十團體$,()項初選：將由六角學院評審進行第一波篩並於/（五）公布佳作單。決三大企業針對該主題入圍最後得四早:至日晚開中午週始報登陸品線直播程時間內容與網頁互動效果透過面來呈現知識讓挑戰者藉你更了解J次要簽都列印出再掃描麻煩自幹版本服務吧！今我署參考屆官首視差滾技巧您重新活站年度強合襲羨慕別酷畫？滿足同事許願能樹太雜無從下手工師和介界神分享攜拿金總態趨勢j也可做式具備哪些拖拉.紹李明塊創辦兼監傳教士~©贊助邱繼緯吳哲宇墨雨努力加載中.網站中收集五顆散落的柏蒂豆，即可獲得驚喜唷立競賽說明關卡任務" \
+	--passthrough-tables \
 /usr/src/app/fonts/Roboto/Roboto-Bold.otf --output-file="/usr/src/app/fonts/Roboto/small-Roboto-Bold.otf"
 
 
